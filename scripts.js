3042d0d443f020a708c0fb921ced9aa909980409
GurinSergey <serheygurin89@gmail.com>
2022-03-14 10:46:17 +0200

    fill content

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 window.onload = () => {
   document.getElementById('r_l').innerHTML = document
     .getElementById('r_l')
-    .innerHTML.padEnd(15, String.fromCharCode(160))
+    .innerHTML.padEnd(14, String.fromCharCode(160))
 
   document.getElementById('phone').innerHTML = document
     .getElementById('phone')
-    .innerHTML.padEnd(8, String.fromCharCode(160))
+    .innerHTML.padEnd(9, String.fromCharCode(160))
 }
