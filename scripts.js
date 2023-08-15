d0bcac4e843ca22bc1f71d1cb4494a76342ea560
Gurin <sgurin.cyg@gmail.com>
2023-08-15 15:46:58 +0300

    translated into English

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 window.onload = () => {
   document.getElementById('r_l').innerHTML = document
     .getElementById('r_l')
-    .innerHTML.padEnd(14, String.fromCharCode(160))
-
-  document.getElementById('phone').innerHTML = document
-    .getElementById('phone')
     .innerHTML.padEnd(9, String.fromCharCode(160))
+
+  document.getElementById('e_l').innerHTML = document
+    .getElementById('e_l')
+    .innerHTML.padEnd(10, String.fromCharCode(160))
 }
